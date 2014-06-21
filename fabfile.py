60fb551a729b967b3b9c650d9cd0abf249e865f2
drmaples <darrell@ordoro.com>
2014-06-21 15:36:35 -0500

    script to fetch and parse gtfs data into json shapes and stops
    - updated data now has MetroRail route that shows Leander stop
    - issue #13

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,174 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+import json
+import tempfile
+import sqlite3
+from collections import defaultdict
+
+import requests
 import fabric.api
+
+
+ROUTE_IDS = [801, 550]  # only routes with realtime data
+GTFS_DOWNLOAD_FILE = os.path.join(tempfile.gettempdir(), 'capmetro_gtfs.zip')
+GTFS_DB = os.path.join(tempfile.gettempdir(), 'capmetro_gtfs_data.db')
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
 def serve():
     fabric.api.local("python -m SimpleHTTPServer 1234")
+
+
+def fetch_gfts_data():
+    print 'fetching gtfs data....'
+    r = requests.get('http://www.gtfs-data-exchange.com/agency/capital-metro/latest.zip', stream=True)
+    assert r.ok, 'problem fetching data. status_code={}'.format(r.status_code)
+
+    with open(GTFS_DOWNLOAD_FILE, 'wb') as f:
+        for chunk in r.iter_content(1024):
+            f.write(chunk)
+    print 'saved to {}'.format(GTFS_DOWNLOAD_FILE)
+
+
+def _get_route_data(curr):
+    sql = '''
+        SELECT shapes.shape_id, count(*) as num_shapes, trips.direction_id, trips.route_id, trips.*
+        FROM
+            shapes,
+            (
+                SELECT *
+                FROM trips, calendar
+                WHERE calendar.service_id = trips.service_id and trips.route_id in ({})
+                GROUP BY trips.shape_id
+            ) as trips
+        WHERE shapes.shape_id = trips.shape_id
+        GROUP BY shapes.shape_id
+        ORDER BY num_shapes DESC
+    '''.format(','.join('?' for _ in ROUTE_IDS))
+    curr.execute(sql, ROUTE_IDS)
+
+    biggest_data_by_route = {}
+    for row in curr:
+        shape_id, num_shapes, direction_id, route_id = row[0], int(row[1]), int(row[2]), row[3]
+        key = (route_id, direction_id)
+        value = {'num_shapes': num_shapes, 'shape_id': shape_id}
+        if key in biggest_data_by_route:
+            if num_shapes > biggest_data_by_route[key]['num_shapes']:
+                biggest_data_by_route[key] = value
+        else:
+            biggest_data_by_route[key] = value
+
+    return biggest_data_by_route
+
+
+def _save_route_data(curr, route_data):
+    for (route_id, direction_id), val in route_data.items():
+        sql = '''
+            SELECT *
+            FROM shapes
+            WHERE shape_id = ?
+            ORDER BY shape_id
+        '''
+        curr.execute(sql, [val['shape_id']])
+
+        data = []
+        for (shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence, shape_dist_traveled) in curr:
+            data.append({
+                'shape_id': shape_id,
+                'shape_pt_lat': shape_pt_lat,
+                'shape_pt_lon': shape_pt_lon,
+                'shape_pt_sequence': shape_pt_sequence,
+                'shape_dist_traveled': shape_dist_traveled,
+            })
+
+        filename = os.path.join(DATA_DIR, 'shapes_{}_{}.json'.format(route_id, direction_id))
+        print 'writing SHAPE data to {}'.format(filename)
+        with open(filename, 'wb') as f:
+            f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')) + '\n')
+
+
+def _save_stop_data(curr):
+    sql = '''
+        SELECT
+            trips.route_id,
+            trips.direction_id,
+            stops.stop_id,
+            stops.stop_code,
+            stops.stop_name,
+            stops.stop_desc,
+            stops.stop_lat,
+            stops.stop_lon,
+            stops.zone_id,
+            stops.stop_url,
+            stops.location_type,
+            stops.parent_station,
+            stops.stop_timezone,
+            stops.wheelchair_boarding,
+            stops.platform_code
+        FROM
+            stop_times, trips, stops
+        WHERE
+            trips.route_id in ({})
+            AND trips.trip_id = stop_times.trip_id
+            AND stop_times.stop_id = stops.stop_id
+        GROUP BY
+            trips.route_id,
+            trips.direction_id,
+            stops.stop_id,
+            stops.stop_code,
+            stops.stop_name,
+            stops.stop_desc,
+            stops.stop_lat,
+            stops.stop_lon,
+            stops.zone_id,
+            stops.stop_url,
+            stops.location_type,
+            stops.parent_station,
+            stops.stop_timezone,
+            stops.wheelchair_boarding,
+            stops.platform_code
+        ORDER BY trips.route_id, trips.direction_id, stops.stop_id
+    '''.format(','.join('?' for _ in ROUTE_IDS))
+    curr.execute(sql, ROUTE_IDS)
+
+    data_by_stops = defaultdict(list)
+    for (route_id, direction_id, stop_id, stop_code, stop_name, stop_desc, stop_lat, stop_lon, zone_id, stop_url, location_type, parent_station, stop_timezone, wheelchair_boarding, platform_code) in curr:
+        data_by_stops[(route_id, direction_id)].append({
+            'route_id': route_id,
+            'direction_id': direction_id,
+            'stop_id': stop_id,
+            'stop_code': stop_code,
+            'stop_name': stop_name,
+            'stop_desc': stop_desc,
+            'stop_lat': stop_lat,
+            'stop_lon': stop_lon,
+            'zone_id': zone_id,
+            'stop_url': stop_url,
+            'location_type': location_type,
+            'parent_station': parent_station,
+            'stop_timezone': stop_timezone,
+            'wheelchair_boarding': wheelchair_boarding,
+            'platform_code': platform_code,
+        })
+
+    for (route_id, direction_id), data in data_by_stops.items():
+        filename = os.path.join(DATA_DIR, 'stops_{}_{}.json'.format(route_id, direction_id))
+        print 'writing STOP data to {}'.format(filename)
+        with open(filename, 'wb') as f:
+            f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')) + '\n')
+
+
+def parse_gtfs_data(force_refetch=True):
+    if int(force_refetch):
+        fetch_gfts_data()
+
+    print 'loading gtfs data into db ({})...'.format(GTFS_DB)
+    fabric.api.local('gtfsdb-load --database_url sqlite:///{} {}'.format(GTFS_DB, GTFS_DOWNLOAD_FILE))
+
+    with sqlite3.connect(GTFS_DB) as conn:
+        curr = conn.cursor()
+        route_data = _get_route_data(curr)
+        _save_route_data(curr, route_data)
+        _save_stop_data(curr)
