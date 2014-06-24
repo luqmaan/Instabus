e3ac4e89a3946f443622e6e3b15b813d17c80a9e
drmaples <darrell@ordoro.com>
2014-06-23 21:02:42 -0500

    all the bower libs wired up via requirejs

--- js/common.js
+++ js/common.js
@@ -1,28 +1,24 @@
 requirejs.config({
     baseUrl: 'js/app',
     paths: {
-        // libs: '../libs',
-        // Served via bower
         'jquery': '../../bower_components/jquery/dist/jquery.min',
         'knockout': '../../bower_components/knockout-dist/knockout.min',
         'underscore': '../../bower_components/underscore/underscore',
-        'x2js': '../../bower_components/x2js/xml2json.min',
-        'when': '../../bower_components/when/when',
+        'xml2json': '../../bower_components/x2js/xml2json.min',
         'leaflet': '../../bower_components/leaflet/dist/leaflet-src',
         'Leaflet.label': '../../bower_components/Leaflet.label/dist/leaflet.label-src'
     },
-    shim: {
-        /*
-        'libs/jquery': {
-            exports: '$'
-        },
-        'libs/xml2json': {
-            exports: 'X2JS'
-        },
-        'libs/underscore': {
-            exports: '_'
+    packages: [
+        {
+            name: 'when',
+            location: '../../bower_components/when',
+            main: 'when'
         }
-        */
+    ],
+    shim: {
+        "xml2json" : {
+          exports : "X2JS"
+        }
     }
 });
 
