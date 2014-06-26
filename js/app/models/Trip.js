7534992b5b020bd0531863ba6ea9e9d68a089ea8
Luqmaan <ldawoodjee@gmail.com>
2014-06-26 06:34:47 -0500

    Use moment js to show a pretty time

--- js/app/models/Trip.js
+++ js/app/models/Trip.js
@@ -1,11 +1,15 @@
-define(['knockout'],
-function(ko) {
+define(['knockout', 'moment'],
+function(ko, moment) {
     function Trip(data) {
         this.tripTime = ko.observable(data.Triptime);
         this.id = ko.observable(data.Tripid);
         this.skedTripID = ko.observable(data.Skedtripid);
         this.block = ko.observable(data.Block);
         this.exception = ko.observable(data.Exception);
+
+        this.moment = ko.computed(function() { return moment(this.tripTime(), 'hh:mm A'); }.bind(this));
+        this.prettyTime = ko.computed(function() { return this.moment().fromNow(); }.bind(this));
+        this.old = ko.computed(function() { return ! this.moment().isAfter(); }.bind(this));
     }
 
     return Trip;
