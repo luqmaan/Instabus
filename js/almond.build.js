5b13b32e02c85f2898725f6323cff7f46cf216b4
Paul Serraino <pserraino99@gmail.com>
2014-07-14 19:16:11 -0500

    using mainConfigFile attr, no more copying and pasting build files

--- js/almond.build.js
+++ js/almond.build.js
@@ -1,21 +1,6 @@
 ({
     baseUrl: '.',
-    paths: {
-        'text': '../bower_components/requirejs-text/text',
-        'jquery': '../bower_components/jquery/dist/jquery.min',
-        'knockout': '../bower_components/knockout-dist/knockout',
-        'underscore': '../bower_components/underscore/underscore',
-        'leaflet': '../bower_components/leaflet/dist/leaflet-src',
-        'Leaflet.label': '../bower_components/Leaflet.label/dist/leaflet.label-src',
-        'moment': '../bower_components/moment/moment',
-        'geolib': '../bower_components/geolib/dist/geolib',
-        'NProgress': '../bower_components/nprogress/nprogress'
-    },
-    packages: [{
-        name: 'when',
-        location: '../bower_components/when',
-        main: 'when'
-    }],
+    mainConfigFile: 'build.js',
     include: 'main',
     insertRequire: ['main'],
     name: '../bower_components/almond/almond',
