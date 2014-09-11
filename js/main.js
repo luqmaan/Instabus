c1afc534a6c8039042283a0b113c92a0e3648e2b
Luqmaan <ldawoodjee@gmail.com>
2014-09-11 18:02:31 -0500

    Update 2014-09-11T23:02:31.638Z

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var ko = require('knockout');
-var Rappid = require('./rappid');
-
-var rappid = window.rappid = new Rappid();
-window.ko = ko;
-
-ko.applyBindings(rappid, document.getElementById('lerappid'));
-rappid.start();
