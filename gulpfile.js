47168e305a6836a3b0554c7cac0b69bb8ffc6b0f
Luqmaan <ldawoodjee@gmail.com>
2014-09-11 12:19:16 -0500

    Some deployment chingas
    - Add versions (fixed for now) to files
    - Build stuff in dist folder
    - Move code to src

--- gulpfile.js
+++ gulpfile.js
@@ -9,44 +9,43 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var clean = require('gulp-clean');
+var replace = require('gulp-replace');
 
-//FIXME: hook this up https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
+// FIXME: hook this up https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
 
 gulp.task('clean', function () {
     return gulp.src('./dist/*.*')
         .pipe(clean());
 });
 
-gulp.task('cssmin', function() {
+gulp.task('build-css', function() {
     var src = [
-        'css/leaflet.css',
-        'css/leaflet-label.css',
-        'css/nprogress.css',
-        'css/progress.css',
-        'css/main.css',
+        './css/leaflet.css',
+        './css/leaflet-label.css',
+        './css/nprogress.css',
+        './css/progress.css',
+        './css/main.css'
     ];
-    return gulp.src(src)
+    return gulp.src(src, {base: './src'})
         .pipe(cssmin())
-        .pipe(concat('./main.min.css'))
-        .pipe(gulp.dest('./css'))  // for local dev
-        .pipe(gulp.dest('./dist'));  // for prod
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('uglify', ['browserify-app'], function() {
-    return gulp.src('./bundle.js')
+    return gulp.src('./dist/js/bundle.js')
         .pipe(sourcemaps.init())
-        .pipe(concat('./bundle.js'))
+        .pipe(concat('./dist/js/bundle.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist'))
-        .pipe(gulp.dest('./')); // FIXME: need to remove this once we figure out deployment
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('browserify-app', function() {
     var bundler = browserify({
-        entries: ['./js/main.js'],
+        entries: ['./src/js/main.js'],
         extensions: ['.js'],
-        debug: true
+        debug: true,
     });
 
     var bundle = function() {
@@ -55,7 +54,7 @@ gulp.task('browserify-app', function() {
             .on('error', function(e) {
                 gutil.log(gutil.colors.red("shit broke", e.toString()));
             })
-            .pipe(source('./bundle.js'))
+            .pipe(source('./dist/js/bundle.js'))
             .pipe(gulp.dest('.'))
             .on('end', function() {
                 gutil.log(gutil.colors.blue("shit finished"));
@@ -65,19 +64,35 @@ gulp.task('browserify-app', function() {
     return bundle();
 });
 
-gulp.task('_serve', ['cssmin', 'browserify-app'], function() {
-    return gulp.src('.')
+gulp.task('build-html', function() {
+    var version = '0.0.1';
+
+    var html = gulp.src('./src/html/index.html')
+        .pipe(replace(/bundle.(css|js)/g, 'bundle.$1?v=' + version))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build-data', function() {
+    var version = '0.0.1';
+
+    gulp.src('./data/**.*')
+        .pipe(gulp.dest('./dist/data'));
+});
+
+gulp.task('_serve', ['build-css', 'browserify-app'], function() {
+    return gulp.src('./dist')
         .pipe(webserver({
-            port: 1234
+            port: 1234,
         })
     );
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./js/**', ['browserify-app']);
-    gulp.watch('./css/**', ['cssmin']);
+    gulp.watch('./src/js/**', ['browserify-app']);
+    gulp.watch('./src/css/**', ['build-css']);
+    gulp.watch('./src/html/**', ['build-html']);
 });
 
-gulp.task('serve', ['cssmin', 'browserify-app', '_serve', 'watch']);
-gulp.task('deploy', ['clean', 'cssmin', 'browserify-app', 'uglify']);
+gulp.task('serve', ['build-data', 'build-css', 'build-html', 'browserify-app', '_serve', 'watch']);
+gulp.task('build', ['clean', 'build-data', 'build-css', 'build-html', 'browserify-app', 'uglify']);
 gulp.task('default', taskListing);
