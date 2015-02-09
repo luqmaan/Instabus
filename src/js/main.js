703443f11a5f55eeed9429a7b33418d81d22e261
Luqmaan <ldawoodjee@gmail.com>
2015-02-09 17:05:58 -0600

    Try loading VehLoc.pb in the browser

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,25 @@ ko.applyBindings(rappid, document.getElementById('lerappid'));
 rappid.start();
 
 window.analytics.identify(fingerme);
+
+
+// var fs = require('fs');
+// var ProtoBuf = require('protobufjs');
+//
+// var protoString = fs.readFileSync(__dirname + '/models/gtfs-realtime.proto');
+//
+// builder = ProtoBuf.loadProto(protoString);
+// root = builder.build();
+// VehiclePosition = root.transit_realtime.VehiclePosition;
+//
+// window.xhr = ProtoBuf.Util.XHR();
+// xhr.open("GET", 'https://data.texas.gov/api/file_data/8CsYrnQIng_pcsZcEJ1BtMyAAFKX1RPZbj0L5j0JS4g?filename=VehLoc.pb', true);
+// // xhr.responseType = "arraybuffer";
+// xhr.onload = function(e) {
+//     console.log('res', xhr.response)
+//     window.e = e
+//     // msg = builder.decode(xhr.response);
+//     // console.log(JSON.stringify(msg, null, 4)); // Correctly decoded
+// }
+// xhr.send(null);
+// console.log(builder);
